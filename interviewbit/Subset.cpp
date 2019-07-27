6fc867db6452e28e7c669a68f0c9f8b18c8a5130
VIVEK RANJAN <ranjanvivek1994@gmail.com>
2019-07-27 22:04:41 +0530

    Update Subset.cpp

--- interviewbit/Subset.cpp
+++ interviewbit/Subset.cpp
@@ -1,3 +1,26 @@
+/*
+Given a set of distinct integers, S, return all possible subsets.
+
+ Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+Also, the subsets should be sorted in ascending ( lexicographic ) order.
+The list is not necessarily sorted.
+Example :
+
+If S = [1,2,3], a solution is:
+
+[
+  [],
+  [1],
+  [1, 2],
+  [1, 2, 3],
+  [1, 3],
+  [2],
+  [2, 3],
+  [3],
+]
+*/
 void subsetGenerate(vector<int> &A, vector<int> temp, int curr, vector<vector<int> > &result) {
     int size = A.size();
     result.push_back(temp);
